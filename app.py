e9cd4a5b3db8754a2e32b6dccfc36b9dce8eb88d
vanduc2514 <vanduc2514@gmail.com>
2022-10-23 23:51:25 +0700

    - Add new category for analysis

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import ingest
 
 identifier = {
-    'meal': ['foody', 'mc donalds', 'dining out', 'taco', 'savico'],
+    'meal': ['foody', 'mc donalds', 'dining out', 'taco', 'savico', 'dinning out', 'meal'],
     'bike': ['moca'],
-    'cafe': ['cafe'],
+    'cafe': ['cafe', 'coffee'],
     'groceries': ['vinmart', 'sieu thi'],
-    'cash_out': ['rut tien mat tai ATM']
+    'cash_out': ['rut tien mat tai ATM'],
+    'house': ['house'],
+    'gift': ['gift']
 }
 
 output = {
@@ -14,20 +16,32 @@ output = {
     'cafe': [],
     'groceries': [],
     'cash_out': [],
+    'house': [],
+    'gift': []
 }
 
-transactions = ingest.vt_report_csv('202209050259.csv')
+undetected = []
 
-for transaction in transactions:
-    transaction_note = transaction.note
+trans = ingest.vt_report_csv('202210232341.csv')
+
+for transaction in trans:
+    transaction_note = transaction.note.lower()
     for keyword, category in identifier.items():
         for item in category:
-            if item.lower() in transaction_note.lower():
+            if item.lower() in transaction_note:
                 output.get(keyword).append(transaction)
-                break
-
+                trans.remove(transaction)
 
 for k, v in output.items():
     print(k)
+    total = 0
     for t in v:
+        print(t)
+        total += t.amount
+    print(total)
+
+leftover = len(trans)
+if leftover > 0:
+    print('Undetected: ', leftover)
+    for t in trans:
         print(t)
